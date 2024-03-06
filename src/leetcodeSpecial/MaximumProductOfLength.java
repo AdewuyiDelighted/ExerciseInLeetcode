bad66cc7e10fdf0226d377f2daa97e7a1fa51fdb
delighted <deborahdelighted5@gmail.com>
2024-03-06 18:57:15 +0100

    MaxlimumProductOfLength

---
+++ src/leetcodeSpecial/MaximumProductOfLength.java
@@ -0,0 +1,5 @@
+package leetcodeSpecial;
+
+public class MaximumProductOfLength {
+
+}
---
+++ test/leetcodeSpecial/MaximumProductOfLengthTest.java
@@ -0,0 +1,7 @@
+package leetcodeSpecial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumProductOfLengthTest {
+
+}
