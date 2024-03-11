15107ad7181a2bdc7f135c9bb44441764b7bcf43
delighted <deborahdelighted5@gmail.com>
2024-03-11 19:28:59 +0100

    linkedlist

--- src/leetcodeSpecial/RemoveNodeFromEndOfTheList.java
+++ src/leetcodeSpecial/RemoveNodeFromEndOfTheList.java
@@ -6,10 +6,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class RemoveNodeFromEndOfTheList {
-    public static LinkedList<Integer> removeNode(LinkedList<Integer> numbers,int target) {
 
-        int nodeToRemove = numbers.size() - target;
-        numbers.remove(nodeToRemove);
+    public static LinkedList<Integer> removeNode(LinkedList<Integer> numbers,int target) {
+        if (!numbers.isEmpty()) {
+            int nodeToRemove = numbers.size() - target;
+            numbers.remove(nodeToRemove);
+            return numbers;
+        }
         return numbers;
     }
 
