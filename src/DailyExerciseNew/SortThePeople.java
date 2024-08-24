2c9ece69b805d09c45fa353694d1fd3ea0abc76a
delighted <deborahdelighted5@gmail.com>
2024-08-24 22:55:59 +0100

    sortThePeople

--- src/DailyExerciseNew/SortThePeople.java
+++ src/DailyExerciseNew/SortThePeople.java
@@ -1,8 +1,34 @@
 package DailyExerciseNew;
 
+import java.util.Arrays;
+
 public class SortThePeople {
+    public static void main(String[] args) {
+        String[] names = {"Mary", "John", "Emma"};
+        int[] heights = {180, 165, 170};
+        String[] results = {"Mary", "Emma", "John"};
+        System.out.println(Arrays.toString(sortThePeople(names, heights)));
+    }
 
     public static String[] sortThePeople(String[] names, int[] heights) {
-        return null;
+        String temps;
+        int temp = 0;
+//        int[] heights = {180, 165, 170};
+
+        for (int height = 0; height < heights.length; height++) {
+            for (int index = 0; index < heights.length; index++) {
+                if (heights[height] > heights[index]) {
+                    temps = names[height];
+                    temp = heights[height];
+                    names[height] = names[index];
+                    names[index] = temps;
+                    heights[height] = heights[index];
+                    heights[index] = temp;
+                }
+            }
+        }
+
+
+        return names;
     }
 }
